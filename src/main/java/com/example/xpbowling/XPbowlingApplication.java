54946395f0018c3cec5b22ed5089a0329dd74491
asgerbirk <asger.birk@gmail.com>
2022-11-21 09:27:08 +0100

    Initial commit

---
+++ src/main/java/com/example/xpbowling/XPbowlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.xpbowling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XPbowlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XPbowlingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xpbowling/XPbowlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xpbowling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XPbowlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
