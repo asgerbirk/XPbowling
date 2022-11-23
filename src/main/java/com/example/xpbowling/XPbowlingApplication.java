999d2fe590d720e61266e40e68b6fc7df180c4f8
asgerbirk <asger.birk@gmail.com>
2022-11-23 11:59:13 +0100

    first

--- src/main/java/com/example/xpbowling/XPbowlingApplication.java
+++ src/main/java/com/example/xpbowling/XPbowlingApplication.java
@@ -1,5 +1,7 @@
 package com.example.xpbowling;
 
+import com.example.xpbowling.bowlingLane.model.BowlingLane;
+import com.example.xpbowling.bowlingLane.repository.BowlingLaneRepository;
 import com.example.xpbowling.equipment.model.*;
 import com.example.xpbowling.equipment.repository.EquipmentRepository;
 import com.example.xpbowling.reservation.model.BowlingReservation;
@@ -12,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class XPbowlingApplication {
@@ -21,7 +24,9 @@ public class XPbowlingApplication {
     }
 
     @Bean
-    public CommandLineRunner importData(ReservationRepository reservationRepository, EquipmentRepository equipmentRepository){
+    public CommandLineRunner importData(ReservationRepository reservationRepository,
+                                        EquipmentRepository equipmentRepository,
+                                        BowlingLaneRepository bowlingLaneRepository){
         return (args -> {
             BowlingReservation reservationbowl1 = new BowlingReservation("Bollamolla", "email", 1200, 1400, 4, 1, 10);
             BowlingReservation reservationbowl2 = new BowlingReservation("Millabolla", "emailena", 1400, 1600, 6, 3, 1, 2, 3);
@@ -53,6 +58,35 @@ public class XPbowlingApplication {
             listOfShoes.add(new Shoes(5, 42));
             listOfShoes.add(new Shoes(5, 46));
             equipmentRepository.saveAll(listOfShoes);
+
+
+            List<BowlingLane> bowlingLaneList = new ArrayList<>();
+            bowlingLaneList.add(new BowlingLane(1,false));
+            bowlingLaneList.add(new BowlingLane(2,false));
+            bowlingLaneList.add(new BowlingLane(3,false));
+            bowlingLaneList.add(new BowlingLane(4,false));
+            bowlingLaneList.add(new BowlingLane(5,false));
+            bowlingLaneList.add(new BowlingLane(6,false));
+            bowlingLaneList.add(new BowlingLane(7,false));
+            bowlingLaneList.add(new BowlingLane(8,false));
+            bowlingLaneList.add(new BowlingLane(9,false));
+            bowlingLaneList.add(new BowlingLane(10,false));
+            bowlingLaneList.add(new BowlingLane(11,false));
+            bowlingLaneList.add(new BowlingLane(12,false));
+            bowlingLaneList.add(new BowlingLane(13,false));
+            bowlingLaneList.add(new BowlingLane(14,false));
+            bowlingLaneList.add(new BowlingLane(15,false));
+            bowlingLaneList.add(new BowlingLane(16,false));
+            bowlingLaneList.add(new BowlingLane(17,false));
+            bowlingLaneList.add(new BowlingLane(18,false));
+            bowlingLaneList.add(new BowlingLane(19,false));
+            bowlingLaneList.add(new BowlingLane(20,false));
+            bowlingLaneList.add(new BowlingLane(21,true));
+            bowlingLaneList.add(new BowlingLane(22,true));
+            bowlingLaneList.add(new BowlingLane(23,true));
+            bowlingLaneList.add(new BowlingLane(24,true));
+            bowlingLaneRepository.saveAll(bowlingLaneList);
+
 
 
 
