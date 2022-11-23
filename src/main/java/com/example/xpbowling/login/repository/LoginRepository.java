8a4cc3da79e440bb96398ad8b297318658dd2a63
Simon <simonffsss@gmail.com>
2022-11-23 08:28:01 +0100

    model structure v2

--- src/main/java/com/example/xpbowling/login/repository/LoginRepository.java
+++ src/main/java/com/example/xpbowling/login/repository/LoginRepository.java
@@ -1,7 +1,7 @@
 package com.example.xpbowling.login.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.xpbowling.login.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface LoginRepository extends JpaRepository<User,Long> {
 }
