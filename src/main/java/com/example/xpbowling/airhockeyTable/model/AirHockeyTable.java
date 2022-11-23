acc6325bad2bf1c01f3b9979ea16e4000eec16db
asgerbirk <asger.birk@gmail.com>
2022-11-23 10:35:30 +0100

    first

--- src/main/java/com/example/xpbowling/airhockeyTable/model/AirHockeyTable.java
+++ src/main/java/com/example/xpbowling/airhockeyTable/model/AirHockeyTable.java
@@ -1,4 +1,29 @@
 package com.example.xpbowling.airhockeyTable.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
 public class AirHockeyTable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int tableNumber;
+
+    public AirHockeyTable(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
 }
